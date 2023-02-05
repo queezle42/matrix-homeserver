267a5d4f13b01aeb704bd9b0f16d9f4ee7d2c519
Jens Nolte <git@queezle.net>
2023-02-05 08:40:47 +0100

    Disable automatic database setup

--- modules/matrix-homeserver/postgresql.nix
+++ modules/matrix-homeserver/postgresql.nix
@@ -8,17 +8,22 @@ in {
     # postgresql database service
     services.postgresql = {
       enable = true;
-      # NOTE: This query will only executed on a fresh postgresql installation.
-      # Manual setup is required when using an existing database.
-      initialScript = pkgs.writeText "synapse-init.sql" ''
-        CREATE USER "matrix-synapse";
-        CREATE DATABASE "matrix-synapse" WITH
-          OWNER "matrix-synapse"
-          TEMPLATE template0
-          ENCODING 'UTF8'
-          LC_COLLATE = 'C'
-          LC_CTYPE = 'C';
-      '';
+      # NOTE: Automatic database has been disabled, so synapse fails with an
+      # empty database. This way synapse cannot start when the database is
+      # empty, e.g. if accidentally started during a postgres migration.
+
+      # This query has to be executed once to set up the database:
+      #initialScript = pkgs.writeText "synapse-init.sql" ''
+      #  CREATE USER "matrix-synapse";
+      #  CREATE DATABASE "matrix-synapse" WITH
+      #    OWNER "matrix-synapse"
+      #    TEMPLATE template0
+      #    ENCODING 'UTF8'
+      #    LC_COLLATE = 'C'
+      #    LC_CTYPE = 'C';
+      #'';
+
+      listen_address.settings.listen_addresses = mkForce "";
     };
 
     # matrix-synapse database configuration
