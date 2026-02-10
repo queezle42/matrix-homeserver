527f82b4cb91f55814baa723953adb9dd69b6b9e
Jens Nolte <git@queezle.net>
2026-02-10 04:44:18 +0100

    element: Remove outdated showLabsSettings settings variant

--- modules/matrix-homeserver/element.nix
+++ modules/matrix-homeserver/element.nix
@@ -21,7 +21,6 @@ let
     disable_3pid_login = true;
 
     defaultCountryCode = "us";
-    showLabsSettings = true;
     show_labs_settings = true;
     enable_presence_by_hs_url = {
       "https://matrix.org" = false;
